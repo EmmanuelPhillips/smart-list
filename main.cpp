66a28468b5b46d1907debfbb2183c4469d62f972
RammusOTP <emmanuelphillips33@gmail.com>
2026-06-08 23:13:27 +0100

    Add node constructor and function stubs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <memory>
 
+// helper struct
 struct Node {
   int value;
   std::unique_ptr<Node> next;
+
+  Node(int v) : value{v}, next{nullptr} {}
+};
+
+class LinkedList {
+private:
+  // unique_ptr to indicate the head of the list.
+  std::unique_ptr<Node> m_head;
+
+public:
+  LinkedList() : m_head{nullptr} {}
+  // no destructor - unique_ptr cleans up after itself.
+
+  void push_front(int value) {}
+  void pop_front() {}
+  void print() const {}
+  bool is_empty() const {}
 };
 
 int main() { return 0; }
